2d7a645d37c446bdded589318c837b4767364088
Galina Zbitneva <galinaknurenko@gmail.com>
2026-01-23 15:27:36 +0300

    added domain files created by https://www.jsonschema2pojo.org/

---
+++ src/main/java/me/domain/Job.java
@@ -0,0 +1,42 @@
+
+package me.domain;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+
+public class Job implements Serializable
+{
+
+    private String title;
+    private String company;
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+    private final static long serialVersionUID = -9178915436796402557L;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
