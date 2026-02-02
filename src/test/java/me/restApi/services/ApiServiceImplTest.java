c4491a4ed06572b0ca91eb312ba2a8cb62be5b18
Galina Zbitneva <galinaknurenko@gmail.com>
2026-02-02 16:39:36 +0300

    added apiService class

---
+++ src/test/java/me/restApi/services/ApiServiceImplTest.java
@@ -0,0 +1,27 @@
+package me.restApi.services;
+
+import me.api.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ApiServiceImplTest {
+    @Autowired
+    ApiService apiService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getUsers() {
+       List<User> users = apiService.getUsers(3);
+        assertEquals(3, users.size());
+    }
+}
